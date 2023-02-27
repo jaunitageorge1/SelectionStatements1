36cc5e98ccb92855f0b46cc7ffa4dcedc05f1efc
jaunitageorge1 <jaunitageorge@gmail.com>
2023-02-26 19:15:32 -0500

    Initial commit

---
+++ SelectionStatements1Final/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SelectionStatementsExercise1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var r = new Random();
+            var favNumber = r.Next(1, 3);
+
+            Console.WriteLine("You -- yes, I'm looking at you. Guess my favorite number (between 1-3)!");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low!!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("You're a wizard!");
+            }
+        }
+    }
+}
